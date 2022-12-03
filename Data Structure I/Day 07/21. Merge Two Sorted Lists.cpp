967e58e5f03b2c03082bcaa483e6f583d8070978
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-12-03 18:49:38 +0530

    Add files via upload

---
+++ Data Structure I/Day 07/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/?envType=study-plan&id=data-structure-i
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+        if(head1 == NULL) return head2;
+        if(head2 == NULL) return head1;
+        if(head1->val > head2->val) std::swap(head1, head2);
+        ListNode *res = head1;
+        while(head1 != NULL and head2 != NULL){
+            ListNode * temp = NULL;
+            while(head1 != NULL and head1->val <= head2->val){
+                temp = head1;
+                head1 = head1->next;
+            }
+            temp->next = head2;
+            std::swap(head1, head2);
+        }
+        return res;
+    }
+};
